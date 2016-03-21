73de146b33e7448d28b2db23e2d944eda6a918f4
YukiOgino <YukiOgino@oginotakeshiki-no-MacBook-Air.local>
2016-03-21 10:30:12 +0900

    リープモーションでトリガーイベント実装
    リープモーションでトリガーイベント実装

--- GoudouGameJam/Assets/scene/TestLeap.cs
+++ GoudouGameJam/Assets/scene/TestLeap.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class TestLeap : MonoBehaviour {
 
+	// 物理演算
+	Rigidbody rigi;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		rigi = gameObject.GetComponent<Rigidbody>();
+		rigi.AddForce(new Vector3(30, 0));
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+
+		// ハンドコントローラー取得
+		GameObject obj = GameObject.Find("HandController");
+		HandController hand = obj.gameObject.GetComponent<HandController>();
+
+		if (hand)
+		{
+			
+			if (hand.isMoveLeft) 
+			{
+				// 左に動かした場合は左に移動
+				rigi.AddForce(new Vector3(-10, 0));
+			}
+			else if (hand.isMoveRight) 
+			{
+				// 右に動かした場合は右に移動
+				rigi.AddForce(new Vector3(10, 0));
+			}
+		}
 	}
 }
