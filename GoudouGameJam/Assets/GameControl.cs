c446105a90394ecc254e0d30f6df5bc34f4dc939
soarcreator <soarcreator@yahoo.co.jp>
2016-03-21 15:25:22 +0900

    test

---
+++ GoudouGameJam/Assets/GameControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+	public int _score = 0;
+	[SerializeField] public Text _scoreText;
+
+	public void addScore(int score)
+	{
+		_score += score;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GoudouGameJam
+
+てすと
